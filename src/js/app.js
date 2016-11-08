0c7ac1814c7ec2369b95d48a150d035094cdb34e
Robby Helms <robert.helms1@gmail.com>
2016-11-08 12:52:55 -0500

    Smooth waves on homepage

--- src/js/app.js
+++ src/js/app.js
@@ -19,4 +19,5 @@
             controller: 'contactCtrl as contact'
           });
         });
+
 })();
--- src/js/controllers/projects.controller.js
+++ src/js/controllers/projects.controller.js
@@ -4,38 +4,53 @@ angular.module('portfolio').controller('projectsCtrl', function(Projects, $state
   var bgWidth = backgroundCanvas.width;
   var bgHeight = backgroundCanvas.height;
   var bgBoxSize = 10;
-  var speed = 3;
+  var speed = 2;
   var time = 0;
+  var mouseMoveEvent;
+  var mouseMove = false;
   if (!Projects.running) {
     Projects.running = true;
     var loopHandle = setInterval(bgDraw, 40);
   }
   function bgDraw() {
-    bgContext.fillStyle = 'rgba(0, 0, 100, 0.1)';
+    bgContext.fillStyle = 'rgba(0, 0, 100, 0.05)';
     bgContext.fillRect(0, 0, bgWidth, bgHeight);
     for (var x = 0; x < bgWidth; x++) {
       var amp1 = (-1 * bgHeight * Math.sin(time * (Math.PI / 180)) / 4);
-      var y1 = bgHeight/2 + amp1 * Math.sin( x * (360 / 700) * (Math.PI / 180) - speed * time / 200);
+      var y1 = bgHeight + amp1 * Math.sin( x * (360 / 700) * (Math.PI / 180) - speed * time / 200);
       var amp2 = (-1 * bgHeight * Math.sin(time * (Math.PI / 180)) / 6);
-      var y2 = bgHeight/2 + amp2 * Math.sin( 2 * x * (360 / 700) * (Math.PI / 180) - speed * time / 400);
+      var y2 = bgHeight + amp2 * Math.sin( 2 * x * (360 / 700) * (Math.PI / 180) - speed * time / 400);
+      var ampMouse = 0;
+      var yMouse = 0;
+      if (mouseMove) {
+        ampMouse = mouseMoveEvent.clientY * (bgHeight / window.innerHeight) - bgHeight/2;
+        yMouse = 1.5 * ampMouse * Math.cos( (x - mouseMoveEvent.clientX * (bgWidth / window.innerWidth) ) * (Math.PI / 180) / 2 );
+      }
       bgContext.fillStyle = 'white';
-      bgContext.fillRect(x, y1, 1, 1);
-      bgContext.fillRect(x, y2, 1, 1);
-      bgContext.fillRect(x, (y1 + y2) / 2 + 5, 1, 1);
-      bgContext.fillRect(x, (2*y2 - y1) - 5, 1, 1);
+      bgContext.fillRect(x, (y1 + yMouse) / 2, 1, 1);
+      bgContext.fillRect(x, (y2 + yMouse) / 2, 1, 1);
+      bgContext.fillRect(x, (y1 + y2 + yMouse) / 3 + 5, 1, 1);
+      bgContext.fillRect(x, (y1 + y2 + yMouse) / 3 - 5, 1, 1);
+      bgContext.fillRect(x, (y1 + y2 + yMouse) / 3 + 10, 1, 1);
     }
+
     time += speed;
     if (time > 3600) {
       time = 0;
     }
   }
 
-  $('.name').on('mouseenter', function() {
-    $('.contactLink a').slideDown();
-  });
-  $('.name').on('mouseleave', function() {
-    setTimeout(function() {
-      $('.contactLink a').slideUp();
-    }, 1000);
+  $('button.navLinks').hover(
+       function(){ // mouseenter
+         $('.linkItems').addClass('active')
+       }
+  )
+
+  $('body').on('mousemove', function(event) {
+    mouseMoveEvent = event;
+    if (mouseMoveEvent.clientX < (window.innerWidth - 200) || mouseMoveEvent.clientY > (200)) { // Hiding nav links
+      $('.linkItems').removeClass('active')
+    }
+    mouseMove = true;
   });
 });
