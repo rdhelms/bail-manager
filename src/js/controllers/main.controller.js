aac8672f5a1e5291cf995769c5224cef6e4b8e54
Robby Helms <robert.helms1@gmail.com>
2017-04-07 20:39:17 -0400

    Added search fields

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -3,6 +3,11 @@ angular.module('baseAngular').controller('mainCtrl', function(InmateService, $st
 
   self.loading = true;
   self.inmateList = [];
+  self.searchChargesType = 'lessThan';
+  self.searchBailType = 'lessThan';
+  self.searchNumCharges;
+  self.searchBailAmount;
+
   InmateService.getInmates().done(function(response) {
     self.inmateList = response;
     self.loading = false;
@@ -22,4 +27,32 @@ angular.module('baseAngular').controller('mainCtrl', function(InmateService, $st
     InmateService.addInmateToManager(inmate).done(function(response) {
       console.log(response);
     });
-  }});
+  };
+
+  self.searchCharges = function(inmate) {
+    if (!self.searchNumCharges) {
+      return true;
+    } else if (self.searchChargesType == 'exact' && inmate.charges.length == self.searchNumCharges) {
+      return true;
+    } else if (self.searchChargesType == 'lessThan' && inmate.charges.length <= self.searchNumCharges) {
+      return true;
+    } else if (self.searchChargesType == 'greaterThan' && inmate.charges.length >= self.searchNumCharges) {
+      return true;
+    }
+    return false;
+  };
+
+  self.searchTotalBail = function(inmate) {
+    if (!self.searchBailAmount) {
+      return true;
+    } else if (self.searchBailType == 'exact' && inmate.totalBailAmount == self.searchBailAmount) {
+      return true;
+    } else if (self.searchBailType == 'lessThan' && inmate.totalBailAmount <= self.searchBailAmount) {
+      return true;
+    } else if (self.searchBailType == 'greaterThan' && inmate.totalBailAmount >= self.searchBailAmount) {
+      return true;
+    }
+    return false;
+  };
+
+});
